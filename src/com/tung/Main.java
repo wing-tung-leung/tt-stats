d51c9494cfaddf0e03cc3b995b8864b25a198d1d
wing tung Leung <tung@zoem.(none)>
2009-11-29 01:42:25 +0100

    Minor cleanup

--- src/com/tung/Main.java
+++ src/com/tung/Main.java
@@ -1,30 +1,24 @@
 package com.tung;
 
-import javax.microedition.lcdui.Alert;
 import javax.microedition.lcdui.Command;
 import javax.microedition.lcdui.CommandListener;
 import javax.microedition.lcdui.Display;
 import javax.microedition.lcdui.Displayable;
 import javax.microedition.lcdui.Form;
-import javax.microedition.lcdui.List;
 import javax.microedition.midlet.MIDlet;
 import javax.microedition.midlet.MIDletStateChangeException;
 
 public class Main extends MIDlet implements CommandListener {
 
 	private Command exitCommand;
-	private Command calculateCommand;
 	
 	private Result result;
 	
-	private Form form;
-	private List overview;
+	private Form enterResultForm;
 	private Display display;
 	private ResultSender resultSender;
 	private ResultView resultView;
 	
-	private Alert alert;
-
 	public Main() {
 		display = Display.getDisplay(this);
 		
@@ -34,7 +28,7 @@ public class Main extends MIDlet implements CommandListener {
 		
 		resultSender = new ResultSender(result);
 		resultView = new ResultView(result, display, resultSender);
-		form = new EnterResultForm(display, result, resultView);
+		enterResultForm = new EnterResultForm(display, result, resultView);
 	}
 
 	protected void destroyApp(boolean arg0) throws MIDletStateChangeException {
@@ -48,12 +42,10 @@ public class Main extends MIDlet implements CommandListener {
 	}
 
 	protected void startApp() throws MIDletStateChangeException {
-		display.setCurrent(form);
+		display.setCurrent(enterResultForm);
 	}
 
 	public void commandAction(Command cmd, Displayable disp) {
-		display.vibrate(500);
-		display.setCurrent(alert, overview);
 		if (cmd == exitCommand) {
 			try {
 				destroyApp(false);
@@ -62,16 +54,7 @@ public class Main extends MIDlet implements CommandListener {
 				e.printStackTrace();
 			}
 			notifyDestroyed();
-		} else if (cmd == calculateCommand) {
-			calculate();
-			display.setCurrent(overview);
 		}
-		display.getDisplay(this).setCurrent(overview);
-	}
-
-	private void calculate() {
-		System.out.println("calculate");
-		overview.append("foobar", null);
 	}
 
 }
